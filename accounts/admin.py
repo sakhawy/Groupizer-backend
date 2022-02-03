4d44482cf213880d9377678f525e3eefe9260353
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-03 06:25:58 +0200

    feat: create User model and related.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+	list_display = ["__str__"]
+
+	fieldsets = (
+		(None, {'fields': ('name', 'email', 'password', 'is_staff', 'is_subbed')}),
+	)
+
+	add_fieldsets = (
+		(None, {'fields': ('email', 'password1', 'password2', 'is_staff', 'is_subbed')}),
+	)
+
+	filter_horizontal = []
+
+	list_filter = []
+	
+	ordering = ['email']
+
+UserModel = get_user_model()
+
+admin.site.register(UserModel, UserAdmin)
