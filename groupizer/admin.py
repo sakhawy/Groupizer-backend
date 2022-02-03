017a7633ddea81e49c5c535d369fafc4319f3346
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-03 06:50:10 +0200

    feat: create 'groupizer' models + add to admin.py

--- groupizer/admin.py
+++ groupizer/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from groupizer import models
+
+class InterestAdmin(admin.ModelAdmin):
+	list_diplay = ["__str__"]
+
+class AdAdmin(admin.ModelAdmin):
+	list_diplay = ["__str__"]
+
+class MembershipAdmin(admin.ModelAdmin):
+	list_diplay = ["__str__"]
+
+class GroupAdmin(admin.ModelAdmin):
+	list_diplay = ["__str__"]
+
+admin.site.register(models.Interest, InterestAdmin)
+admin.site.register(models.Ad, AdAdmin)
+admin.site.register(models.Membership, MembershipAdmin)
+admin.site.register(models.Group, GroupAdmin)
--- groupizer/models.py
+++ groupizer/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Interest(models.Model):
+	title = models.CharField(max_length=50)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="interests")
+
+	def __str__(self):
+		return self.title
+
+class Group(models.Model):
+	title = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.title
+
+class Membership(models.Model):
+	PENDING = "PENDING"
+	REJECTED = "REJECTED"
+	MEMBER = "MEMBER"
+	ADMIN = "ADMIN"
+	ROLES = (
+		(PENDING, "Pending"),
+		(REJECTED, "Rejected"),
+		(MEMBER, "Member"),
+		(ADMIN, "Admin"),
+	)
+
+	role = models.CharField(max_length=30, choices=ROLES)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="memberships")
+	group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="memberships")
+
+	def __str__(self):
+		return self.user.email
+
+class Ad(models.Model):
+	title = models.CharField(max_length=150)
+	description = models.TextField()
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="ads")
+	group = models.OneToOneField(Group, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+# class Chat(models.Model):
+# 	pass
