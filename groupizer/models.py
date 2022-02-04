d47dde26229c4ef9ae811b6eb414236bf59bfe5c
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-04 21:11:08 +0200

    fix: fix rel on Interest + fixes

--- groupizer/models.py
+++ groupizer/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class Interest(models.Model):
 	title = models.CharField(max_length=50)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="interests")
+	users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="interests", blank=True, null=True)
 
 	def __str__(self):
 		return self.title
--- groupizer/views.py
+++ groupizer/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from groupizer import models, serializers
 
 class AdViewSet(viewsets.ModelViewSet):
@@ -9,3 +11,20 @@ class AdViewSet(viewsets.ModelViewSet):
 class InterestViewSet(viewsets.ModelViewSet):
 	queryset = models.Interest.objects.all()
 	serializer_class = serializers.InterestSerializer
+
+	@action(methods=["post"], detail=False)
+	def add(self, request):
+		interest_ids = request.data.get('id')
+
+		if type(interest_ids) != list:
+			interest_ids = [interest_ids]
+		
+		for interest_id in interest_ids:
+			try:
+				interest = models.Interest.objects.get(id=interest_id)
+				interest.users.add(request.user)
+			except:
+				return Response(status=status.HTTP_400_BAD_REQUEST)
+		user_interests = models.Interest.objects.filter(users=request.user)
+		user_interests_serializer = serializers.InterestSerializer(user_interests, many=True)
+		return Response(user_interests_serializer.data, status=status.HTTP_200_OK)
