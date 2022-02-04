88f8c4747698a46098f9e861be2cb95c16061d82
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-04 18:31:58 +0200

    feat: add djangorestframework

---
+++ groupizer/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
+
+from groupizer import views
+
+router = routers.DefaultRouter()
+# router.register(r'playlists', views.PlaylistViewSet)
+# router.register(r'videos', views.VideoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
