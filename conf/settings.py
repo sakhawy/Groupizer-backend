aaab209b609d81eae038f3d0aa184df3147e0c43
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-07 10:21:21 +0200

    fix: add django-cors-headers + db alteration

--- conf/settings.py
+++ conf/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 	"rest_framework",
+	"corsheaders",
 
 	"groupizer",
 	"accounts"
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+	"corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,3 +146,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
 	'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
