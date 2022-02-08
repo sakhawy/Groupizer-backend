824761cf4b9a2b51566171298fa9f33b244a89e0
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-08 17:01:55 +0200

    fix: make 'user' nested in MessageSerializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
 from chat import models
+from accounts.serializers import UserSerializer
 
 class MessageSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.Message
 		fields = '__all__'
+
+	user = UserSerializer(read_only=True)
 
 class ChatSerializer(serializers.ModelSerializer):
 	class Meta:
