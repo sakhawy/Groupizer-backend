ed198dc22ce869c8aeff098aba27a139c231f1cf
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-16 20:11:38 +0200

    fix: get token from subprotocol

--- conf/middlewares.py
+++ conf/middlewares.py
@@ -6,12 +6,8 @@ from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
 
 @database_sync_to_async
-def get_user(user_model, token_key):
-	print(token_key['user_id'])
-	try:
-		return user_model.objects.get(id=token_key['user_id'])
-	except user_model.DoesNotExist:
-		return AnonymousUser()
+def get_user(jwt, token_key):
+	return jwt.get_user(token_key)
 
 class TokenAuthMiddleware(BaseMiddleware):
 	"""
@@ -25,9 +21,12 @@ class TokenAuthMiddleware(BaseMiddleware):
 	async def __call__(self, scope, receive, send):
 		try:
 			authorization_header = dict(scope['headers']).get(b'authorization', None)
+			if authorization_header == None:
+				authorization_header = bytes("Bearer " + scope['subprotocols'][0], encoding="utf-8")
+				print(authorization_header)
 			assert authorization_header != None
 			validated_token = self.jwt.get_validated_token(self.jwt.get_raw_token(authorization_header))
 		except:
 			validated_token = None
-		scope['user'] = AnonymousUser() if validated_token is None else await get_user(self.user_model, validated_token)
+		scope['user'] = AnonymousUser() if validated_token is None else await get_user(self.jwt, validated_token)
 		return await super().__call__(scope, receive, send)
